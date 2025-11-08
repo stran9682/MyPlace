aa92f22d5d74bf4cd5903a0f3fa70c799f1d9379
Chris <cliques_steams_3x@icloud.com>
2025-11-08 14:21:35 -0500

    matches page template and various other changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Loginpage } from './pages/Login-page'
 import { Signuppage } from './pages/Signup-page'
+import { Matchespage } from './pages/Matches-page'
+
 
 import './App.css' 
 import type { ReactElement } from 'react'
@@ -15,6 +17,8 @@ function App(): ReactElement {
         <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Loginpage/>} />
         <Route path="/signup" element={<Signuppage/>} />
+        <Route path="/matches" element={<Matchespage/>} />
+
       </Routes>
     </main>
     </>
