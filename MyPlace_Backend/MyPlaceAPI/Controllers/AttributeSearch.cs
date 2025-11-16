0614338ee3d8aadfeec59163005313bf906f807a
LegionYata <ly996437@wcupa.edu>
2025-11-16 13:42:59 -0500

    Revert "Getting the Project up to date"
    This reverts commit 5dd196876ca56a9dbc5db8d35521c4a72a7dcc48.

---
+++ MyPlace_Backend/MyPlaceAPI/Controllers/AttributeSearch.cs
@@ -0,0 +1,53 @@
+using DataLibrary;
+using Microsoft.AspNetCore.Mvc;
+using MyPlaceAPI.Services;
+
+namespace MyPlaceAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AttributeSearch : ControllerBase
+{
+    private readonly ElasticService _elasticService;
+    
+    public AttributeSearch(ElasticService elasticService)
+    {
+        _elasticService = elasticService;
+    }
+    
+    [HttpPost("create-index")]
+    public async Task<IActionResult> CreateIndex(string indexName)
+    {
+        await _elasticService.CreateIndex(indexName);
+        return Ok();
+    }
+
+    [HttpPost("add-user")]
+    public async Task<IActionResult> AddProfileAttributes([FromBody] ProfileAttributes profileAttributes)
+    {
+        var result = await  _elasticService.AddOrUpdate(profileAttributes);
+        return result ? Ok() : BadRequest();
+    }
+
+    [HttpGet("get-user")]
+    public async Task<IActionResult> GetProfileAttributes(string key)
+    {
+        var user = await _elasticService.GetAttributes(key);
+        return user != null ? Ok(user) : NotFound();
+    }
+
+    [HttpGet("get-all-users")]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var users = await _elasticService.GetAllAttributes();
+        Console.WriteLine($"Found {users?.Count ?? 0} users");
+        return users != null ? Ok(users) : NotFound();
+    }
+
+    [HttpDelete("delete-user")]
+    public async Task<IActionResult> DeleteProfileAttributes(string key)
+    {
+        var result = await _elasticService.Delete(key);
+        return result ? Ok() : BadRequest();
+    }
+}
