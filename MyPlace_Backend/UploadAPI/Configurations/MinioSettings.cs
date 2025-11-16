5dd196876ca56a9dbc5db8d35521c4a72a7dcc48
LegionYata <ly996437@wcupa.edu>
2025-11-16 13:36:47 -0500

    Getting the Project up to date

--- MyPlace_Backend/UploadAPI/Configurations/MinioSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace UploadAPI.Configurations;
-
-public class MinioSettings
-{
-    public string Endpoint { get; set; }
-    public string AccessKey { get; set; }
-    public string SecretKey { get; set; }
-    public string BucketName { get; set; }
-}
