68b8352fd93e82b8f42a364dd6cf9a4478dc6722
Sebastian Tran <transebastian21@gmail.com>
2025-11-24 10:14:40 -0500

    Starting chathub

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import Header from '../Components/Header'
 import LandingContent from '../Components/LandingContent'
 
+import signalRService from "../../services/SignalRService";
+//await signalRService.StartConnection()
 
 export const Home = () => {
     return <>
--- MyPlace_Backend/ChatHub/Hubs/ChatHub.cs
+++ MyPlace_Backend/ChatHub/Hubs/ChatHub.cs
@@ -1,13 +1,31 @@
+using System.Security.Claims;
+using DataLibrary;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
 
 namespace ChatHub.Hubs;
 
-// [Authorize]
+[Authorize]
 public class ChatHub : Hub
 {
+    private readonly ProfileContext _profileContext;
+    
+    public ChatHub(ProfileContext profileContext)
+    {
+        _profileContext = profileContext;
+    }
+    
     public override async Task OnConnectedAsync()
     {
-        Console.WriteLine(Context.User.Identity.Name);
+       await _profileContext.Groups
+            .Where(group => group.Profiles.Select(profile => profile.Id)
+                .Contains(Context.User.FindFirst(ClaimTypes.Name).Value))
+            .Select(group => group.GroupName)
+            .ToListAsync();
+       
+       Console.WriteLine(Context.User.FindFirst(ClaimTypes.Name).Value);
     }
+    
+    
 }
